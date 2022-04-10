712e8941ac69656a9f267924d0eb5fc2808f845e
Timmeey86 <Timmeey86@hotmail.com>
2022-04-10 22:16:03 +0200

    Increases version to 0.1

--- Plugin/version.h
+++ Plugin/version.h
@@ -1,8 +1,8 @@
 #pragma once
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 0
-#define VERSION_PATCH 1
-#define VERSION_BUILD 435
+#define VERSION_MINOR 1
+#define VERSION_PATCH 0
+#define VERSION_BUILD 436
 
 #define stringify(a) stringify_(a)
 #define stringify_(a) #a
