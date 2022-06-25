97ecd7042775f9975b0e6d4da74bbccfed26e889
Timmeey86 <Timmeey86@hotmail.com>
2022-06-25 23:12:24 +0200

    Revert "Adds test code for calling from another plugin"
    This reverts commit 72070016785c82b4b518841fc5915c00b13ef1db.

--- Plugin/RLTrainingTimer.h
+++ Plugin/RLTrainingTimer.h
@@ -8,8 +8,6 @@
 #include <training/ui/TrainingProgramFlowControlUi.h>
 
 #include "version.h"
-#include <API/RLTrainingTimerAPI.h>
-#include <DLLImportExport.h>
 
 constexpr auto plugin_version = stringify(VERSION_MAJOR) "." stringify(VERSION_MINOR) "." stringify(VERSION_PATCH) "." stringify(VERSION_BUILD);
 
@@ -22,10 +20,6 @@ public:
 	virtual void onLoad();
 	virtual void onUnload();
 
-	/** Retrieves the API for adding training programs through code. */
-	RLTrainingTimerAPI* getAPI() const;
-
 private:
-	RLTrainingTimerAPI* _api;
 };
 
--- Plugin/version.h
+++ Plugin/version.h
@@ -1,11 +1,8 @@
 #pragma once
-
-// !!! CHANGING THE MAJOR VERSION REQUIRES UPDATING THE PREJUMP PLUGIN AS WELL !!!
 #define VERSION_MAJOR 1
-
 #define VERSION_MINOR 0
 #define VERSION_PATCH 0
-#define VERSION_BUILD 600
+#define VERSION_BUILD 599
 
 #define stringify(a) stringify_(a)
 #define stringify_(a) #a
